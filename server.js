81e5cbff11c9cffc93164197f52daefde61ceb71
seanawilkinson-coder <seanawilkinson@gmail.com>
2026-04-16 14:33:09 -0400

    Debug: alert + Permissions-Policy header for print

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Allow window.print() through Railway's proxy
+app.use((req, res, next) => {
+  res.setHeader('Permissions-Policy', 'print=*');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
